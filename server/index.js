628bfed5255d60b856e1a35816f30fd5b6abd678
SrdjanCoric <srdjan.coric1984@gmail.com>
2021-06-08 19:23:04 +0200

    add allow methods

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
