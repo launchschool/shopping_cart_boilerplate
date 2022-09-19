e94ee8095e5c811d1a004021ff62fd0d49414c22
SrdjanCoric <srki.coric@gmail.com>
2022-09-19 07:48:47 +0200

    added client

---
+++ client/mockData/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Amazon Kindle E-reader",
+    quantity: 5,
+    price: 79.99
+  },
+  {
+    id: 2,
+    title: "Apple 10.5-Inch iPad Pro",
+    quantity: 0,
+    price: 649.99
+  },
+  {
+    id: 3,
+    title: "Yamaha Portable Keyboard",
+    quantity: 2,
+    price: 155.99
+  },
+  {
+    id: 4,
+    title: "Tinker, Tailor, Soldier, Spy - A John le Carre Novel",
+    quantity: 12,
+    price: 13.74
+  }
+];
