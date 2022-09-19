8ba5fbc79769c01e0f0692b6b8e2ac34adba7407
Cody Williams <61119166+Code-yWilliams@users.noreply.github.com>
2022-09-19 11:44:20 -0700

    oops

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import productData from '../mockData/data';
 
-
 const Product = ({title, price, quantity }) => {
 
   const h3title = React.createElement("h3", {key: 1}, title);
