cb31747a00d13bb3f84cb8ae78014bddb1465ed5
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-09-20 13:54:23 -0500

    Refactor toggling display of AddProductForm

--- client/components/AddProductForm.js
+++ client/components/AddProductForm.js
@@ -3,22 +3,22 @@ import { useState } from "react";
 const AddProductForm = () => {
   const [formHidden, setFormHidden] = useState(true);
 
-  const handleAddProductClick = () => {
+  const handleFormToggleVisibleClick = () => {
     setFormHidden(!formHidden);
   };
 
   return (
-    <div className="add-form">
-      <p hidden={!formHidden}>
+    <div className={formHidden ? "add-form" : "add-form.visible"}>
+      <p>
         <a
           className="button add-product-button"
-          onClick={handleAddProductClick}
+          onClick={handleFormToggleVisibleClick}
         >
           Add A Product
         </a>
       </p>
-      <h3 hidden={formHidden}>Add Product</h3>
-      <form hidden={formHidden}>
+      <h3>Add Product</h3>
+      <form>
         <div className="input-group">
           <label htmlFor="product-name">Product Name</label>
           <input type="text" id="product-name" />
@@ -33,7 +33,7 @@ const AddProductForm = () => {
         </div>
         <div className="actions form-actions">
           <a className="button">Add</a>
-          <a className="button" onClick={handleAddProductClick}>
+          <a className="button" onClick={handleFormToggleVisibleClick}>
             Cancel
           </a>
         </div>
