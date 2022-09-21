40a6bda5cf391dd61b895d44412b74443ece3fb0
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-09-21 10:09:29 -0500

    Added product edit form (no hiding yet)

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,5 +1,4 @@
 const Product = ({ title, price, quantity }) => {
-
   return (
     <div className="product">
       <div className="product-details">
@@ -13,6 +12,29 @@ const Product = ({ title, price, quantity }) => {
         <a className="delete-button">
           <span>X</span>
         </a>
+      </div>
+      <div className="edit-form">
+        <h3>Edit Product</h3>
+        <form>
+          <div className="input-group">
+            <label htmlFor="product-name">Product Name</label>
+            <input type="text" id="product-name" value={title}></input>
+          </div>
+          <div className="input-group">
+            <label htmlFor="product-price">Price</label>
+            <input type="text" id="product-price" value={price}></input>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="product-quantity">Quantity</label>
+            <input type="text" id="product-quantity" value={quantity}></input>
+          </div>
+
+          <div className="actions form-actions">
+            <a className="button">Update</a>
+            <a className="button">Cancel</a>
+          </div>
+        </form>
       </div>
     </div>
   );
