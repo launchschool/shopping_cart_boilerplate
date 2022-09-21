99ae8721f24e0f5a94ca9c6d7521a3fec539c70d
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-09-21 10:51:35 -0500

    implemented edit-form-toggling

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const Product = ({ title, price, quantity }) => {
+  const [editFormVisible, setEditFormVisible] = useState(false);
+
+  const handleEditLinkClick = (event) => {
+    event.preventDefault();
+    setEditFormVisible(!editFormVisible);
+  };
+
   return (
     <div className="product">
       <div className="product-details">
@@ -7,35 +16,41 @@ const Product = ({ title, price, quantity }) => {
         <p className="quantity">{quantity} left in stock</p>
         <div className="actions product-actions">
           <a className="button add-to-cart">Add to Cart</a>
-          <a className="button edit">Edit</a>
+          <a onClick={handleEditLinkClick} className="button edit">
+            Edit
+          </a>
         </div>
         <a className="delete-button">
           <span>X</span>
         </a>
       </div>
-      <div className="edit-form">
-        <h3>Edit Product</h3>
-        <form>
-          <div className="input-group">
-            <label htmlFor="product-name">Product Name</label>
-            <input type="text" id="product-name" value={title}></input>
-          </div>
-          <div className="input-group">
-            <label htmlFor="product-price">Price</label>
-            <input type="text" id="product-price" value={price}></input>
-          </div>
+      {editFormVisible ? (
+        <div className="edit-form">
+          <h3>Edit Product</h3>
+          <form>
+            <div className="input-group">
+              <label htmlFor="product-name">Product Name</label>
+              <input type="text" id="product-name" value={title}></input>
+            </div>
+            <div className="input-group">
+              <label htmlFor="product-price">Price</label>
+              <input type="text" id="product-price" value={price}></input>
+            </div>
 
-          <div className="input-group">
-            <label htmlFor="product-quantity">Quantity</label>
-            <input type="text" id="product-quantity" value={quantity}></input>
-          </div>
+            <div className="input-group">
+              <label htmlFor="product-quantity">Quantity</label>
+              <input type="text" id="product-quantity" value={quantity}></input>
+            </div>
 
-          <div className="actions form-actions">
-            <a className="button">Update</a>
-            <a className="button">Cancel</a>
-          </div>
-        </form>
-      </div>
+            <div className="actions form-actions">
+              <a className="button">Update</a>
+              <a onClick={() => setEditFormVisible(false)} className="button">
+                Cancel
+              </a>
+            </div>
+          </form>
+        </div>
+      ) : null}
     </div>
   );
 };
