bff0961aa3006a32f695668709998e0c2b0ce14b
Srdjan Coric <28871727+SrdjanCoric@users.noreply.github.com>
2024-05-20 14:06:36 +0200

    Update data.js
    add mockCart

--- client/mockData/data.js
+++ client/mockData/data.js
@@ -1,28 +1,41 @@
-const mockData = [
+export const mockProducts = [
   {
     id: 1,
     title: "Amazon Kindle E-reader",
     quantity: 5,
-    price: 79.99
+    price: 79.99,
   },
   {
     id: 2,
     title: "Apple 10.5-Inch iPad Pro",
     quantity: 0,
-    price: 649.99
+    price: 649.99,
   },
   {
     id: 3,
     title: "Yamaha Portable Keyboard",
     quantity: 2,
-    price: 155.99
+    price: 155.99,
   },
   {
     id: 4,
     title: "Tinker, Tailor, Soldier, Spy - A John le Carre Novel",
     quantity: 12,
-    price: 13.74
-  }
+    price: 13.74,
+  },
 ];
 
-export default mockData;
+export const mockCart = [
+  {
+    id: 1,
+    title: "Amazon Kindle E-reader",
+    quantity: 1,
+    price: 79.99,
+  },
+  {
+    id: 2,
+    title: "Apple 10.5-Inch iPad Pro",
+    quantity: 3,
+    price: 649.99,
+  },
+];
